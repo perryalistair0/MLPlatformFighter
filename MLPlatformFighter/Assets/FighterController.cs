34d708dff3ee3b577e0b548f57d3d749b302846e
perryalistar <perryalistair0@gmail.com>
2022-03-15 14:25:47 +0000

    Init

---
+++ MLPlatformFighter/Assets/FighterController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterController : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimatorStateInfo animInfo = anim.GetCurrentAnimatorStateInfo(0);
+        if(animInfo.IsName("Base")){
+            if (Input.GetKeyDown(KeyCode.J)) anim.SetTrigger("Punch");
+            if (Input.GetKeyDown("space")) anim.SetTrigger("Jump");
+        }
+        if(Input.GetKey(KeyCode.D)) {
+            anim.SetInteger("Condition", 2);
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            anim.SetInteger("Condition", 3);
+        }
+        if(!(Input.GetKey(KeyCode.A) ||Input.GetKey(KeyCode.D) )){
+            anim.SetInteger("Condition", 0);
+        }
+    }
+}
